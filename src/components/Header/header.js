301029583f668149102b086738483d57f23f3dd7
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-08 21:06:21 +0300

    add header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,14 +1,11 @@
 import './header.css';
-// import { NavLink, useNavigate } from 'react-router-dom';
 
 function Header() {
-    // const navigate = useNavigate();
-
-
     return (
-        <header className="header">
-          
-        </header>
+        <div className="header">
+            <img src="logo.svg" alt="Logo" className="logo" />
+            <div className="account">Account</div>
+        </div>
     );
 }
 
