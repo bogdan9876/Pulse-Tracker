d2d2dba4b671c9cc09af60cfc82b9b205fbdf2c6
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-09 16:31:51 +0300

    next commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -41,7 +41,7 @@ function Home() {
           PRESS
         </div>
       </div>
-      <ul className='uls'>
+      <ul className="uls">
         {data.map((item, index) => {
           const createdAt = new Date(item.created_at);
           const date = `${createdAt.getDate()}.${createdAt.getMonth() + 1}.${createdAt.getFullYear()}`;
@@ -49,17 +49,21 @@ function Home() {
 
           return (
             <li key={index} className="liStyle">
-              {item.heart_rate}
-              <div>
-              {date}
+              <div className='date'>
+                {date}
+                <div className='time'>
+                  {time}
+                </div>
               </div>
-              <div>
-              {time}
+              <div className="heartRateContainer">
+                <span className="heartRate">{item.heart_rate}</span>
+                <img src="liheart.svg" alt="Heart" className="heartIcon" />
               </div>
             </li>
           );
         })}
       </ul>
+
     </div>
   );
 }
