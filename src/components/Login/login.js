eddd556a4f40021e79fd482a5cbd088a21f4921d
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-09 18:47:46 +0300

    next commit

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -39,8 +39,10 @@ const Login = () => {
         onSubmit={handleLogin}
       >
         <Form className="input-container">
+          <p className='text'>Hello</p>
           <Field className="login-input" type="text" name="email" placeholder="Your email" />
           <ErrorMessage name="email" component={ErrorValid} />
+          <p className='text'>Hello</p>
           <Field className="login-input" type="password" name="password" placeholder="*******" />
           <ErrorMessage name="password" component={ErrorValid} />
           <button className="login-button" type="submit">Login</button>
