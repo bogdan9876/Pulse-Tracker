cb02c93c2f55cd2016b56f5c0fb939a2164487e3
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-15 01:04:31 +0300

    connect doctors to database

--- src/components/Doctor/doctor.js
+++ src/components/Doctor/doctor.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../Header/header';
 import './doctor.css';
 
 const Doctor = () => {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/doctors');
+                setDoctors(response.data);
+            } catch (error) {
+                console.error('Error fetching doctors:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <Header />
@@ -12,42 +28,41 @@ const Doctor = () => {
                     <h2 className="title">Available doctors</h2>
                 </div>
                 <div className="elements-doctors">
-                    <div className="divided1">
-                        <div className="leftSection1">
-                            <div className="item">
-                                <span className="itemText1">Name</span>
-                                <span className="itemNumber">mama Sani</span>
+                    {doctors.map((doctor, index) => (
+                        <div key={index} className="divided1">
+                            <div className="leftSection1">
+                                <div className="item">
+                                    <span className="itemText1">Name</span>
+                                    <span className="itemNumber">{doctor.name}</span>
+                                </div>
+                                <div className="item">
+                                    <span className="itemText1">Position</span>
+                                    <span className="itemNumber">{doctor.position}</span>
+                                </div>
+                                <div className="item">
+                                    <span className="itemText1">Experience</span>
+                                    <span className="itemNumber">{doctor.experience}</span>
+                                </div>
+                                <div className="item">
+                                    <span className="itemText1">Location</span>
+                                    <span className="itemNumber">{doctor.location}</span>
+                                </div>
+                                <div className="quote">
+                                    <span className="quoteText">"</span>
+                                    <p className="quoteContent">{doctor.description}</p>
+                                </div>
                             </div>
-                            <div className="item">
-                                <span className="itemText1">position</span>
-                                <span className="itemNumber">Cardiologist</span>
-                            </div>
-                            <div className="item">
-                                <span className="itemText1">experience</span>
-                                <span className="itemNumber">10+ years</span>
-                            </div>
-                            <div className="item">
-                                <span className="itemText1">location</span>
-                                <span className="itemNumber">Lviv</span>
-                            </div>
-                            <div className="quote">
-                                <span className="quoteText">"</span>
-                                <p className="quoteContent"> Experienced cardiologist specializing
-                                    in the diagnosis and treatment of heart diseases.
-                                    Provides comprehensive care to patients, ensuring
-                                    the highest level of medical assistance.</p>
-                            </div>
-                        </div>
-                        <div className="rightSection15">
-                            <img src="/doctors/doctor2.jpg" alt="Extra" className="extraImage" />
-                            <div className="learnMoreContainer5">
-                                <div className="learnMore5">
-                                    <p className="learnMoreText5">Sign up for a visit</p>
-                                    <img src="/arrow2.svg" alt="Arrow" className="arrowIcon" />
+                            <div className="rightSection15">
+                                <img src={`/doctors/${doctor.image}`} alt="Doctor" className="extraImage" />
+                                <div className="learnMoreContainer5">
+                                    <div className="learnMore5">
+                                        <p className="learnMoreText5">Sign up for a visit</p>
+                                        <img src="/arrow2.svg" alt="Arrow" className="arrowIcon" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </>
@@ -55,4 +70,3 @@ const Doctor = () => {
 }
 
 export default Doctor;
-
