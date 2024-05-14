374677c7c3fc65736070691b6b900077e7257ac9
bogdan9876 <bogdankryveiko@gmail.com>
2024-05-15 00:24:30 +0300

    add detalied info

--- src/components/Doctor/doctor.js
+++ src/components/Doctor/doctor.js
@@ -11,39 +11,43 @@ const Doctor = () => {
                 <div className="titleContainer">
                     <h2 className="title">Available doctors</h2>
                 </div>
-                <div  className="elements-doctors">
-                <div className="divided1">
-                    <div className="leftSection1">
-                        <div className="item">
-                            <span className="itemText1">position</span>
-                            <span className="itemNumber">Cardiologist</span>
+                <div className="elements-doctors">
+                    <div className="divided1">
+                        <div className="leftSection1">
+                            <div className="item">
+                                <span className="itemText1">Name</span>
+                                <span className="itemNumber">mama Sani</span>
+                            </div>
+                            <div className="item">
+                                <span className="itemText1">position</span>
+                                <span className="itemNumber">Cardiologist</span>
+                            </div>
+                            <div className="item">
+                                <span className="itemText1">experience</span>
+                                <span className="itemNumber">10+ years</span>
+                            </div>
+                            <div className="item">
+                                <span className="itemText1">location</span>
+                                <span className="itemNumber">Lviv</span>
+                            </div>
+                            <div className="quote">
+                                <span className="quoteText">"</span>
+                                <p className="quoteContent"> Experienced cardiologist specializing
+                                    in the diagnosis and treatment of heart diseases.
+                                    Provides comprehensive care to patients, ensuring
+                                    the highest level of medical assistance.</p>
+                            </div>
                         </div>
-                        <div className="item">
-                            <span className="itemText1">experience</span>
-                            <span className="itemNumber">10+ years</span>
-                        </div>
-                        <div className="item">
-                            <span className="itemText1">location</span>
-                            <span className="itemNumber">Lviv</span>
-                        </div>
-                        <div className="quote">
-                            <span className="quoteText">"</span>
-                            <p className="quoteContent"> Experienced cardiologist specializing
-                             in the diagnosis and treatment of heart diseases.
-                              Provides comprehensive care to patients, ensuring 
-                              the highest level of medical assistance.</p>
-                        </div>
-                    </div>
-                    <div className="rightSection15">
-                        <img src="/doctors/doctor1.jpg" alt="Extra" className="extraImage" />
-                        <div className="learnMoreContainer5">
-                            <div className="learnMore5">
-                                <p className="learnMoreText5">Sign up for a visit</p>
-                                <img src="/arrow2.svg" alt="Arrow" className="arrowIcon" />
+                        <div className="rightSection15">
+                            <img src="/doctors/doctor2.jpg" alt="Extra" className="extraImage" />
+                            <div className="learnMoreContainer5">
+                                <div className="learnMore5">
+                                    <p className="learnMoreText5">Sign up for a visit</p>
+                                    <img src="/arrow2.svg" alt="Arrow" className="arrowIcon" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </>
