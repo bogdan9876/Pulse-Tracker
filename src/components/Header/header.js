34f7a75d028f6f11d352c87fef3993c48bb307ad
bogdan9876 <bogdankryveiko@gmail.com>
2024-07-04 19:16:17 +0300

    complete header dark mode

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,5 @@
 import './header.css';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../../store';
 
@@ -9,17 +9,17 @@ function Header() {
 
     const handleSunClick = () => {
         dispatch(toggleTheme());
-      };
+    };
 
     return (
         <div className={`header ${isDarkMode ? 'dark' : ''}`}>
-            <NavLink exact to="/"><img src="logo.svg" alt="Logo" className="logo" /></NavLink>
+            <NavLink exact to="/"><img src={isDarkMode ? '/logo-dark.svg' : '/logo.svg'} alt="Logo" className="logo" /></NavLink>
             <div className="account">
-            <NavLink to="/doctor" className="header-chat" >Doctors</NavLink>
-            <NavLink to="/chat" className="header-chat" >ChatAI(demo)</NavLink>
-            <NavLink to="/profile" className="header-account" >Account</NavLink>
+                <NavLink to="/doctor" className={`header-chat ${isDarkMode ? 'dark' : ''}`} >Doctors</NavLink>
+                <NavLink to="/chat" className={`header-chat ${isDarkMode ? 'dark' : ''}`} >ChatAI(demo)</NavLink>
+                <NavLink to="/profile" className={`header-chat ${isDarkMode ? 'dark' : ''}`} >Account</NavLink>
             </div>
-            <img src={isDarkMode ? '/sun.svg' : '/moon.svg'} onClick={handleSunClick}/>
+            <img src={isDarkMode ? '/sun.svg' : '/moon.svg'} onClick={handleSunClick} />
         </div>
     );
 }
